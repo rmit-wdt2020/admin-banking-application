4bd15ef40cb9bf53b78d71c56eac1981a222cf9e
Vineet <s3734938@student.rmit.edu.au>
2020-02-08 14:33:52 +1100

    Chnages for login to logout(incomplete)

--- AngularAdminPage/ClientApp/src/app/login/login.component.ts
+++ AngularAdminPage/ClientApp/src/app/login/login.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule, EventEmitter, Injectable, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {Router} from '@angular/router';
+import { NavMenuComponent } from '../../../src/app/nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+@Injectable({
+  providedIn: 'root'
+})
 export class LoginComponent implements OnInit {
-  public loggedIn = false;
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onChange = new EventEmitter<string>();
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -16,8 +21,9 @@ export class LoginComponent implements OnInit {
 
   onSubmit(form: NgForm) {
     if (form.value.AdminID === 'admin' && form.value.Password === 'admin') {
+      sessionStorage.setItem('loggedIn', 'true');
+      this.onChange.emit('true');
       this.router.navigate(['\customer-list']);
-      this.loggedIn = true;
     }
-}
-}
+    }
+  }
--- AngularAdminPage/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ AngularAdminPage/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Injectable, NgModule, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginComponent } from '../../../src/app/login/login.component';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+@Injectable({
+  providedIn: 'root'
+})
+@NgModule({
+  providers: [ LoginComponent ]
+})
+export class NavMenuComponent implements OnInit {
+  @Input()
+  loggedIn: any;
   isExpanded = false;
+  constructor(private router: Router, private loginService: LoginComponent) {
+    this.loginService.onChange.subscribe(x => {
+      // this.loggedIn =  sessionStorage.getItem('loggedIn');
+      console.log('here');
+  });
+  }
+
+  ngOnInit() {
+    sessionStorage.setItem('loggedIn', 'false');
+    this.loggedIn =  sessionStorage.getItem('loggedIn');
+    this.loginService.onChange.subscribe(x => {
+      // this.loggedIn =  sessionStorage.getItem('loggedIn');
+      console.log('here');
+  });
+  }
+
+  logout() {
+    sessionStorage.setItem('loggedIn', 'false');
+    this.router.navigate(['\login']);
+    this.loggedIn =  sessionStorage.getItem('loggedIn');
+  }
+
+  login() {
+    this.router.navigate(['\login']);
+  }
 
   collapse() {
     this.isExpanded = false;
